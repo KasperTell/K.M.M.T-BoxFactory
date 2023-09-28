89075c58d55554b146fb19a6d46563027ae3c844
KasperTell <kaspertell@gmail.com>
2023-09-28 10:36:01 +0200

    Ændring af port og tilføjelse af datamodel

---
+++ infrastructure/DataModel/Box.cs
@@ -0,0 +1,11 @@
+﻿namespace DefaultNamespace;
+
+public class Box
+{
+    public int boxId { get; set; }
+    public string productName { get; set;  }
+    public int width { get; set; }
+    public int height { get; set; }
+    public int length { get; set; }
+    public string boxImgUrl { get; set; }
+}
