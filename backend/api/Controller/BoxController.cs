ed996653fd4144807c04049bf33e39e3504b7d8a
KasperTell <kaspertell@gmail.com>
2023-10-03 09:25:52 +0200

    Catching exceptions and setting MinLength

--- backend/api/Controller/BoxController.cs
+++ backend/api/Controller/BoxController.cs
@@ -17,42 +17,89 @@ public class BoxController : ControllerBase
     [Route("/box/all")]
     public IEnumerable<Box> getAllBoxes()
     {
-        return _boxService.getAllBoxes();
+        try
+        {
+            return _boxService.getAllBoxes();
+        }
+        catch (Exception e)
+        {
+           Console.WriteLine(e);
+           throw new Exception("Error when getting all boxes", e);
+        }
     }
 
     [HttpGet]
     [Route("/box/{boxId}")]
     public Box getBoxById([FromRoute] int boxId)
     {
-        return _boxService.getBoxById(boxId);
+        try
+        {
+            return _boxService.getBoxById(boxId);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Error when getting box based on ID", e);
+        }
     }
 
     [HttpGet]
     [Route("/box")]
     public IEnumerable<Box> SearchBoxes([FromQuery] string searchQuery )
     {
-        return _boxService.SearchBoxes(searchQuery);
+        try
+        {
+            return _boxService.SearchBoxes(searchQuery);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Error when searching", e);
+        }
     } 
 
     [HttpPost]
     [Route("/box")]
     public Box post([FromBody] Box box)
     {
-        return _boxService.createBox(box.product_name, box.width, box.height, box.length, box.box_img_url);
+        try
+        {
+            return _boxService.createBox(box.product_name, box.width, box.height, box.length, box.box_img_url);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Error when creating a box", e);
+        }
     }
 
     [HttpPut]
     [Route("/box/{boxId}")]
     public Box put([FromRoute] int boxId, [FromBody] Box box)
     {
-        return _boxService.updateBox(boxId, box.product_name, box.width, box.height, box.length, box.box_img_url);
+        try
+        {
+            return _boxService.updateBox(boxId, box.product_name, box.width, box.height, box.length, box.box_img_url);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Error when updating a box", e);
+        }
     }
 
     [HttpDelete]
     [Route("/box/{boxId}")]
     public void delete([FromRoute] int boxId)
     {
-        _boxService.deleteBox(boxId);
+        try
+        {
+            _boxService.deleteBox(boxId);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Error when deleting a box", e);
+        }
     }
-    
 }
--- backend/infrastructure/DataModel/Box.cs
+++ backend/infrastructure/DataModel/Box.cs
@@ -1,13 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DefaultNamespace;
 
 public class Box
 {
     public int box_id { get; set; }
+    [MinLength(3)]
     public string product_name { get; set;  }
+    
     public int width { get; set; }
     public int height { get; set; }
     public int length { get; set; }
+    [MinLength(5)]
     public string box_img_url { get; set; }
 }
