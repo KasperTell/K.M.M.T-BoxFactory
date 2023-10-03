e46f23a4d158011e7d60d0489090d7364b8bccc3
KasperTell <kaspertell@gmail.com>
2023-10-03 09:55:46 +0200

    fungerende miljø variabel

--- backend/infrastructure/Utilities.cs
+++ backend/infrastructure/Utilities.cs
@@ -1,10 +1,8 @@
 ﻿namespace infrastructure;
 public class Utilities
 {
-    //private static readonly Uri Uri = new Uri(Environment.GetEnvironmentVariable("pgconn")!);
-
-    private static readonly Uri Uri =
-        new Uri("postgres://ivgcputr:qWMbqWS6xdH4V6920px_bAEF0n971sP6@snuffleupagus.db.elephantsql.com/ivgcputr");
+    private static readonly Uri Uri = new Uri(Environment.GetEnvironmentVariable("pgconn")!);
+    
     public static readonly string
         ProperlyFormattedConnectionString = string.Format(
             "Server={0};Database={1};User Id={2};Password={3};Port={4};Pooling=true;MaxPoolSize=3",
@@ -13,5 +11,4 @@ public class Utilities
             Uri.UserInfo.Split(':')[0],
             Uri.UserInfo.Split(':')[1],
             Uri.Port > 0 ? Uri.Port : 5432);
-
 }
