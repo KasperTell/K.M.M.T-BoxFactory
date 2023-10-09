254ac80e7b35d22c32cd2d004502103b8eae89ee
brunstighanhval <113671720+brunstighanhval@users.noreply.github.com>
2023-10-09 10:35:00 +0200

    Test

---
+++ TestNUnit/DeleteBox.cs
@@ -0,0 +1,36 @@
+﻿namespace TestNUnit;
+
+[TestFixture]
+public class DeleteBox
+{
+    private BoxController _boxController;
+
+    [SetUp]
+    public void Setup()
+    {
+        _boxController = new BoxController();
+    }
+
+    [Test]
+    public void Delete_ValidBoxId_ReturnsNoContent()
+    {
+        // Arrange
+        int boxId = 123;
+
+        // Act
+        var result = _boxController.delete(boxId);
+
+        // Assert
+        Assert.That(result, Is.TypeOf<NoContentResult>());
+    }
+
+    [Test]
+    public void Delete_InvalidBoxId_ThrowsException()
+    {
+        // Arrange
+        int boxId = -1;
+
+        // Assert
+        Assert.Throws<Exception>(() => _boxController.delete(boxId));
+    }
+}
