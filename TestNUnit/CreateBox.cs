b9764ba52a5b692347f06fbdb5197b6e52ddb2d0
brunstighanhval <113671720+brunstighanhval@users.noreply.github.com>
2023-10-10 12:18:33 +0200

    Test box api

--- TestNUnit/CreateBox.cs
+++ TestNUnit/CreateBox.cs
@@ -15,10 +15,10 @@ namespace Playwright;
 public class CreateTests : PageTest
 {
     //Here we're using the entire stack from UI and down (using Playwright)
-    [TestCase("Lille", "28", "7824", "1746", "LilleBox.com")]
-    [TestCase("Mellem", "28", "7824", "1746", "MellemBox.com")]
-    [TestCase("Stor", "28", "7824", "1746", "StorBox.com")]
-    [TestCase("MegetStor", "28", "7824", "1746", "MegetStorBox.com")]
+    [TestCase("Lille", 28, 2828, 2828, "LilleBox.com")]
+    [TestCase("Mellem", 28, 2828, 2828, "MellemBox.com")]
+    [TestCase("Stor", 28, 2828, 2828, "StorBox.com")]
+    [TestCase("MegetStor", 28, 2828, 2828, "MegetStorBox.com")]
 
     public async Task BoxCanSuccessfullyBeCreatedFromUi(string product_name, int width, int height, int length, string box_img_url)
     {
@@ -55,10 +55,10 @@ public class CreateTests : PageTest
         
     }
     
-    [TestCase("Lille", "28", "7824", "1746", "LilleBox.com")]
-    [TestCase("Mellem", "28", "7824", "1746", "MellemBox.com")]
-    [TestCase("Stor", "28", "7824", "1746", "StorBox.com")]
-    [TestCase("MegetStor", "28", "7824", "1746", "MegetStorBox.com")]
+    [TestCase("Lille", 2828, 2828, 2828, "LilleBox.com")]
+    [TestCase("Mellem", 2828, 2828, 2828, "MellemBox.com")]
+    [TestCase("Stor", 2828, 2828, 2828,  "StorBox.com")]
+    [TestCase("MegetStor", 2828, 2828, 2828, "MegetStorBox.com")]
 
     public async Task BoxCanSuccessfullyBeCreatedFromHttpClient(string product_name, int width, int height, int length, string box_img_url)
     {
@@ -81,21 +81,6 @@ public class CreateTests : PageTest
         {
             conn.QueryFirst<Box>("SELECT * FROM BoxFactory.box;").Should().BeEquivalentTo(boxFromResponseBody); //Should be equal to article found in DB
         }
-    }
-    //When the form validation is violated, the submit cannot be clicked
-    [TestCase("Stor", "11", "7824", "821", "StorBox.com")]
-    [TestCase("Meget Stor", "21", "0", "1746", "MegetStorBox.com")]
-
-    public async Task BoxCanSuccessfullyBeCreatedFromHttpRequest(string product_name, int width, int height, int length, string box_img_url)
-    {
-        await Page.GotoAsync(Helper.ClientAppBaseUrl);
-        await Page.GetByTestId("create_button").ClickAsync();
-        await Page.GetByTestId("create_product_name_form").Locator("input").FillAsync(product_name);
-        await Page.GetByTestId("create_width_form").Locator("input").FillAsync(width.ToString());
-        await Page.GetByTestId("create_height_form").Locator("input").FillAsync(height.ToString());
-        await Page.GetByTestId("create_length_form").Locator("input").FillAsync(length.ToString());
-        await Page.GetByTestId("create_box_img_url_form").Locator("input").FillAsync(box_img_url);
-        await Expect(Page.GetByTestId("create_submit_form")).ToHaveAttributeAsync("aria-disabled", "true");
     }
 
     //Here we're testing that the API returns a bad request response and no article is created when bad values are sent
